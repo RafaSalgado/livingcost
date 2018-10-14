2e905bae70c8bcb4bbdac81adceb17dbb620f183
rafasalgado <rasalgadol@unal.edu.co>
2018-10-14 06:54:04 -0500

    ca

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func UpdateLivingcostEndPoint(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	respondWithJson(w, http.StatusOK, livingcost)x|
+	respondWithJson(w, http.StatusOK, livingcost)
 }
 
 // DELETE an existing livingcost
